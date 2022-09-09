fc42c55d0bae6b5d2fb9a11f56bc666fc374d8d7
samdamy <samdammy82@gmail.com>
2022-09-09 04:34:19 -0700

    bulding a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * * main - Entry point
- *   Description: a program that print string
- * * Return: 0
- * */
+ * main - Entry point
+ * Return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
