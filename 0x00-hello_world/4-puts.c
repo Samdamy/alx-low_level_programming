620a39fb6a895b83fc83839df9f56dc631158f4e
samdamy <samdammy82@gmail.com>
2022-09-09 01:09:17 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+touch 4-puts.c
+
+#include <stdio.h>
+
+/**
+ *
+ * * main - main block
+ *
+ * * Return: 0
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
