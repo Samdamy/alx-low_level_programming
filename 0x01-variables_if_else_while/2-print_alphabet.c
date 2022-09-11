8906814edad6fbb3925d238c06f3bd0c04446556
samdamy <samdammy82@gmail.com>
2022-09-11 16:45:21 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Block
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char start;
+	for (start = 'a'; start  <= 'z'; start++)
+{
+	putchar(start);
+}
+	putchar('\n');
+return (0)
+}
