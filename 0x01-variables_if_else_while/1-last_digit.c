4c19fc6df9309ee0265b9df3f16e79dea4f1a8e8
samdamy <samdammy82@gmail.com>
2022-09-10 22:59:35 -0700

    1 last digit c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,8 @@
  * main - print the letters of the alphabet                                                                  
  * Description: print the letters of the alphabet                                                            
  * Return: Always 0 (Success)                                                                                
- */                                                                                                          
-int main(void)                                                                                               
-                                                                                                             
+ */                                                                                                        
+int main(void)	
 {                                                                                                            
         int i = 97;                                                                                          
         while (i < 123)                                                                                      
