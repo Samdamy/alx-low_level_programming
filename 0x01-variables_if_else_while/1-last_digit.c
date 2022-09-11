dae7798b72247e53d708295169b50b1f8dc846be
samdamy <samdammy82@gmail.com>
2022-09-11 08:34:17 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,29 @@
-#include <stdio.h>                                                                                           
-/**                                                                                                          
- * main - print the letters of the alphabet                                                                  
- * Description: print the letters of the alphabet                                                            
- * Return: Always 0 (Success)                                                                                
- */                                                                                                        
-int main(void)	
-{                                                                                                            
-        int i = 97;                                                                                          
-        while (i < 123)                                                                                      
-{                                                                                                            
-        putchar(i);                                                                                          
-        i++;                                                                                                 
-}                                                                                                            
-        putchar(10);                                                                                 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (if successfull)
+ */
+int main(void)
+{
+	int n;
+	int ld;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	ld = n % 10;
+	if (ld > 5)
+{
+	printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+}
+	else if (ld < 6 && ld != 0)
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld)
+}
+	else
+{
+	printf("Last digit of %d is %d and is 0\n", n, ld);
+}
 return (0);
-} 
+}
+
