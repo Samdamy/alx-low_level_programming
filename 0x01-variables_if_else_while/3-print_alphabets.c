a05a38369cbbba6366f4bf2b837ac6895895fada
samdamy <samdammy82@gmail.com>
2022-09-11 18:32:18 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - block
+ * main - Block
  *
- * Return: 0 always
+ * Return: 0 if successful
+ *
  */
 
- int main(void)
- {
- 	for (char set1 = 'a'; set1 <= 'z'; set1++)
+int main(void)
+{
+	for (char set1 = 'a'; set1 <= 'z'; set1++)
+	char set1;
 
-		char set1;
+	char set2;
 
-		char set2;
-
-		for (set1 = 'a'; set1 <= 'z'; set1++)
-			putchar(set1);
-			
-		for (char set2 = 'A'; set2 <= 'Z'; set2++)
-
-		for (set2 = 'A'; set2 <= 'Z'; set2++)
-			putchar(set2);
-
-		putchar('\n');
-return (0);
-} 
+	for (set1 = 'a'; set1 <= 'z'; set1++)
+		putchar(set1);
+	for (char set2 = 'A'; set2 <= 'Z'; set2++)
+	for (set2 = 'A'; set2 <= 'Z'; set2++)
+		putchar(set2);
+	putchar('\n');
+	return (0);
+}
