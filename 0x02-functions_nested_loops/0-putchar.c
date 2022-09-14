9c88a56e0cdbd2b069b4a75285d215e187941bb0
samdamy <samdammy82@gmail.com>
2022-09-13 21:10:13 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+include "main.h"
+/**
+ * main - entry block
+ * Description: Print _purchar followed by a new line
+ * The program should 0
+ * return: 0
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+0x02-function-nested_loops
+#Nested loops
+#Function
+#Declaration
+#Prototype
+#Scope of 
