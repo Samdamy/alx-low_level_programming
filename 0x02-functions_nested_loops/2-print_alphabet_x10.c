d8666bd101252fe90dbc4dd4e7d5c8cd76f9d7c8
samdamy <samdammy82@gmail.com>
2022-09-14 09:17:08 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,16 @@
  * Description - prints the alphabets in lowercase 10x
  * Return
  */
+
 void print-alphabet_x10(void)
+
 {
 	char c;
 	int i = 0;
 
-	while (i<=9)
+	while (i <= 9)
 	{
-		for (c = 'a';c <= 'z';c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
