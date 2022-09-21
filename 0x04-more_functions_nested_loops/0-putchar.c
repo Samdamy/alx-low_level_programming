a54e134a79b15bf7ae599d3319128dce76b31fb1
samdamy <samdammy82@gmail.com>
2022-09-20 20:12:42 -0700

    0-putchar.c

---
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+
+	int item;
+
+	for (item = 0; item < 8; item++)
+		_putchar(str[item]);
+	_putchar('\n');
+
+	return (0);
+
+}
