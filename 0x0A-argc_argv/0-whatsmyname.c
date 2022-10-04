84aff5cae15d454b4ac996a4533dc30634fd6d1e
samdamy <samdammy82@gmail.com>
2022-10-03 20:56:40 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  * @argv: argument vector of pointers to string
  * Return: always 0
  */
-int main(int argc _attribute_((unused)). char *argv[])
+int main(int argc, char *argv[])
 {
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
