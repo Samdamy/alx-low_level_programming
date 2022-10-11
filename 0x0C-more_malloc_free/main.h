089ee05c254baab772a27023c813a6a7c001ae8c
samdamy <samdammy82@gmail.com>
2022-10-10 17:54:25 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,16 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FINE_MAIN
 
-void _puts(cahr *str);
 void *malloc_checked(unsigned int b);
+int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _putchar(char c);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
 
-
-#sendif
-
+#endif
